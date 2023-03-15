c1d0a1ef692513f21f751806e56321e2408f7a23
AhmedEid3 <ahmed.eid3@outlook.com>
2023-03-15 13:06:31 +0200

    Make QueueArray Generic

--- src/ds/QueueArray.java
+++ src/ds/QueueArray.java
@@ -2,14 +2,14 @@ package ds;
 
 import java.util.Arrays;
 
-public class QueueArray {
-    private int[] queue;
+public class QueueArray<T> {
+    private Object[] queue;
     private int count = 0;
     private int front = 0;
     private int rear = 0;
 
     public QueueArray(int capacity) {
-        queue = new int[capacity];
+        queue = (T[]) new Object[capacity];
     }
 
     public boolean isEmpty() {
@@ -20,7 +20,7 @@ public class QueueArray {
         return count == queue.length;
     }
 
-    public void enqueue(int item) {
+    public void enqueue(T item) {
         if (isFull()) throw new IllegalArgumentException("Queue is full");
 
         queue[rear] = item;
@@ -28,26 +28,25 @@ public class QueueArray {
         count++;
     }
 
-    public int dequeue() {
+    public T dequeue() {
         if (isEmpty()) throw new IllegalStateException("Queue is empty");
 
         var item = queue[front];
-        queue[front] = 0;
+        queue[front] = (T) null;
         front = (front + 1) % queue.length;
         count--;
 
-        return item;
+        return (T) item;
     }
 
-    public int peek() {
+    public T peek() {
         if (isEmpty()) throw new IllegalStateException("Queue is empty");
 
-        return queue[front];
+        return (T) queue[front];
     }
 
     @Override()
     public String toString() {
         return Arrays.toString(queue);
     }
-
 }
